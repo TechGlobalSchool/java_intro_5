9cdad6f7679f76179cdfaacedf8ccda44c958694
TechGlobalSchool <tgschool01@gmail.com>
2022-09-14 20:24:22 -0500

    ...

---
+++ src/mathClass/AbsoluteMethod.java
@@ -0,0 +1,30 @@
+package mathClass;
+
+import java.util.Scanner;
+
+public class AbsoluteMethod {
+
+    public static void main(String[] args) {
+
+        System.out.println("Difference between your numbers is = " + Math.abs(10 - 14));
+        System.out.println("Difference between your numbers is = " + Math.abs(14 - 10));
+        System.out.println("Subtraction between your numbers is = " + (10 - 14));
+
+        /*
+        Write a program asks user two ages and gets the difference between ages
+        and prints them
+
+        Output:
+        "Please enter an age"
+        input -> 24
+        "Please enter another age"
+        input -> 32
+
+        "Difference between your ages is = 8"
+         */
+
+        Scanner numberTaker = new Scanner(System.in);
+
+
+    }
+}
