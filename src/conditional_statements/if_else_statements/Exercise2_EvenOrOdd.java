03d57ac339b8d504ea2ecb39878ed96c5f803c40
TechGlobalSchool <tgschool01@gmail.com>
2022-09-18 11:59:56 -0500

    added if else statements practice

--- src/conditional_statements/if_else_statements/Exercise2_EvenOrOdd.java
+++ src/conditional_statements/if_else_statements/Exercise2_EvenOrOdd.java
@@ -1,5 +1,7 @@
 package conditional_statements.if_else_statements;
 
+import java.util.Scanner;
+
 public class Exercise2_EvenOrOdd {
     public static void main(String[] args) {
         /*
@@ -20,5 +22,19 @@ public class Exercise2_EvenOrOdd {
 
         Program: The number you entered is odd!
          */
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a number?");
+        int num = input.nextInt();
+
+        if(num % 2 == 1){
+            System.out.println("The number you entered is odd!");
+        }
+        else{
+            System.out.println("The number you entered is even!");
+        }
+
+        System.out.println("End of the program");
     }
 }
