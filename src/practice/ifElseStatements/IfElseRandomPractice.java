193986075763e87725d4d9d72afedc0e6326c9fd
TechGlobalSchool <tgschool01@gmail.com>
2022-09-21 20:06:21 -0500

    ...

--- src/practice/ifElseStatements/IfElseRandomPractice.java
+++ src/practice/ifElseStatements/IfElseRandomPractice.java
@@ -2,7 +2,7 @@ package practice.ifElseStatements;
 
 public class IfElseRandomPractice {
     public static void main(String[] args) {
-
+        System.out.println("\nTASK1\n");
         /*
         1. Random number generate it +
         2. If else, ternary, sout
@@ -26,5 +26,34 @@ public class IfElseRandomPractice {
 
         //Ternary Solution
         System.out.println((myRandomNumber >= 10 && myRandomNumber <= 25) ? "true" : "false");
+
+        // Printing the BOOLEAN expression
+        System.out.println((myRandomNumber >= 10 && myRandomNumber <= 25));
+
+
+        System.out.println("\nTASK2\n");
+
+        int mySecondRandom = (int)(Math.random() * (100) + 1); // 1-100
+
+        //10
+        if (mySecondRandom <= 50){
+            // 1-50
+            System.out.println(mySecondRandom + " is in the 1st half");
+            if (mySecondRandom <= 25){
+                System.out.println(mySecondRandom + " is in the 1st quarter");
+            }else{
+                System.out.println(mySecondRandom + " is in the 2nd quarter");
+            }
+
+        }else {
+            //51-100
+            System.out.println(mySecondRandom + " is in the 2nd half");
+            if (mySecondRandom <= 75){
+                System.out.println(mySecondRandom + " is in the 3rd quarter");
+            }else {
+                System.out.println(mySecondRandom + " is in the 4th quarter");
+            }
+        }
+
     }
 }
