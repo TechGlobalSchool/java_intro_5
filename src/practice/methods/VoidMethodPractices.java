c1d9466c6027c110486f94af8ae20f9cb4618b33
TechGlobalSchool <tgschool01@gmail.com>
2022-10-04 19:33:26 -0500

    ..

--- src/practice/methods/VoidMethodPractices.java
+++ src/practice/methods/VoidMethodPractices.java
@@ -2,9 +2,20 @@ package practice.methods;
 
 public class VoidMethodPractices {
     public static void main(String[] args) {
-        VoidMethodPractices.flavorPicker("chocolate");
-        VoidMethodPractices.flavorPicker("Chocolate");
-        VoidMethodPractices.flavorPicker("strawberry");
+//        VoidMethodPractices.flavorPicker("chocolate");
+//        VoidMethodPractices.flavorPicker("Chocolate");
+//        VoidMethodPractices.flavorPicker("strawberry");
+//
+//        VoidMethodPractices.checkAge(19);
+//        VoidMethodPractices.checkAge(20);
+//        VoidMethodPractices.checkAge(25);
+//        VoidMethodPractices.checkAge(-25);
+
+        VoidMethodPractices.checkCreditScore(99.34);
+        VoidMethodPractices.checkCreditScore(1099.34);
+        VoidMethodPractices.checkCreditScore(2099.34);
+        VoidMethodPractices.checkCreditScore(5099.34);
+        VoidMethodPractices.checkCreditScore(-5099.34);
     }
 
     /*
@@ -40,6 +51,49 @@ public class VoidMethodPractices {
             default:
                 System.out.println("Give me a valid flavor!");
                 break;
+        }
+    }
+
+    /*
+    Create a public static method named as "checkAge", it is going to take an age as parameter
+    then prints according to following:
+    -> 0 - 19 -> You are a teenager
+    -> 20 - 21 -> You can drive
+    -> more than 21 -> You can drink coke
+     */
+
+    public static void checkAge(int age){
+        if (age >= 0 && age <= 19){
+            System.out.println("You are a teenager");
+        }else if (age == 20 || age == 21) {// age >= 20 && age <= 21
+            System.out.println("You can drive");
+        } else if (age > 21){
+            System.out.println("You can drink coke");
+        }else {
+            System.out.println("Give me a valid age!");
+        }
+    }
+
+    /*
+    Create a public static method named as "checkCreditScore", it is going to take a balance as parameter
+    then it will print according to following:
+
+    balance:
+    0.00 - 999.99 -> Low credit score
+    1000.00 - 1999.99 -> Medium credit score
+    2000.00 - 2999.99 -> Good credit score
+    else -> No valid credit score!
+     */
+
+    public static void checkCreditScore(double balance){
+        if (balance >= 0.00 && balance < 1000.00){
+            System.out.println("Low credit score");
+        } else if (balance >= 1000.00 && balance < 2000.00){
+            System.out.println("Medium credit score");
+        } else if (balance >= 2000.00 && balance < 3000.00){
+            System.out.println("Good credit score");
+        } else {
+            System.out.println("No valid credit score!");
         }
     }
 }
