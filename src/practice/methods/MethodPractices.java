7c6d3820d1ace25deba83aad2d38a24c82c73411
TechGlobalSchool <tgschool01@gmail.com>
2022-10-05 20:12:14 -0500

    ..

--- src/practice/methods/MethodPractices.java
+++ src/practice/methods/MethodPractices.java
@@ -2,35 +2,39 @@ package practice.methods;
 
 public class MethodPractices {
     public static void main(String[] args) {
-        System.out.println();
-        printStringDesc("John");
-        System.out.println();
-        printStringDesc("");
-        System.out.println();
-        printStringDesc("JavaScript");
-        System.out.println();
-        printStringDesc("Scrpt");
+//        System.out.println();
+//        printStringDesc("John");
+//        System.out.println();
+//        printStringDesc("");
+//        System.out.println();
+//        printStringDesc("JavaScript");
+//        System.out.println();
+//        printStringDesc("Scrpt");
+
+        MethodPractices.printMiddle("toyota"); // yo
+        MethodPractices.printMiddle("civic"); // v
+        MethodPractices.printMiddle("ci"); // Length is less than 3"
     }
 
-    public static void printStringDesc(String str){
-        /*
-        is empty -> "Length is zero"
-        else
-            print the length
-            print the first char
-            print the last char
-            check if it has vowels
-         */
+    /*
+      is empty -> "Length is zero"
+      else
+          print the length
+          print the first char
+          print the last char
+          check if it has vowels
+       */
+    public static void printStringDesc(String str) {
 
         // "JavaScript",  some action --> 't' index 9, length 10
         // "Java",  some action --> 'a' index 3, length 4
 
-        if (str.isEmpty()){
+        if (str.isEmpty()) {
             System.out.println("Length is zero");
-        }else{
+        } else {
             System.out.println("Length is = " + str.length() +
                     "\nFirst char is = " + str.charAt(0) +
-                    "\nLast char is = " + str.charAt(str.length() -1)//str.substring(str.length() -1)
+                    "\nLast char is = " + str.charAt(str.length() - 1)//str.substring(str.length() -1)
             );
 
             // a, e, i, o, u, A, E, I, O, U
@@ -44,13 +48,29 @@ public class MethodPractices {
         }
     }
 
-    public static void printHasVowel(String str){
+    public static void printHasVowel(String str) {
         // a, e, i, o, u, A, E, I, O, U
         if (str.toLowerCase().contains("a") || str.toLowerCase().contains("e") || str.toLowerCase().contains("i")
-                || str.toLowerCase().contains("o") || str.toLowerCase().contains("u")){
+                || str.toLowerCase().contains("o") || str.toLowerCase().contains("u")) {
             System.out.println("This String has vowel");
-        }else{
+        } else {
             System.out.println("This String doesn't have vowel");
+        }
+    }
+
+    public static void printMiddle(String str) {
+        if (str.length() < 3) {
+            System.out.println("Length is less than 3");
+        } else {
+            /*
+            if the length is even get the middle 2 chars
+            else get the middle char
+             */
+            if (str.length() % 2 == 0) { // toyota -> yo - 2,3 == length 6  | length /2 - 1 = 2
+                System.out.println(str.substring(str.length() / 2 - 1, str.length() / 2 + 1)); // 2, 4
+            } else { // civic -> v - 2 == length 5  | length /2 = 2
+                System.out.println(str.charAt(str.length() / 2));
+            }
         }
     }
 }
