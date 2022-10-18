2be0c99bedb5789e513e1c5cb41eb89921cab81a
Tech Global <tgschool01@gmail.com>
2022-10-18 18:42:40 -0500

    ...

---
+++ src/loops/characters/LoopsCharactersPractices.java
@@ -0,0 +1,7 @@
+package loops.characters;
+
+public class LoopsCharactersPractices {
+    public static void main(String[] args) {
+        System.out.println("We are learning loops with characters");
+    }
+}
