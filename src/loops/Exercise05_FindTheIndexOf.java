42f10e965b6c868e5954fe9b3027c90e1fda2d94
Tech Global <tgschool01@gmail.com>
2022-10-19 19:16:05 -0500

    ..

--- src/loops/Exercise05_FindTheIndexOf.java
+++ src/loops/Exercise05_FindTheIndexOf.java
@@ -10,10 +10,22 @@ public class Exercise05_FindTheIndexOf {
 //        System.out.println("1. " + findFirstIndexOf("I love Java", 'a'));
 //        System.out.println("2. " + findFirstIndexOf("Banana", 'a'));
 //        System.out.println("3. " + findFirstIndexOf("Banana", 't'));
+//
+//        System.out.println("1. " + findLastIndexOf("I love Java", 'a'));
+//        System.out.println("2. " + findLastIndexOf("Banana", 'a'));
+//        System.out.println("3. " + findLastIndexOf("Banana", 't'));
 
-        System.out.println("1. " + findLastIndexOf("I love Java", 'a'));
-        System.out.println("2. " + findLastIndexOf("Banana", 'a'));
-        System.out.println("3. " + findLastIndexOf("Banana", 't'));
+//        System.out.println("1. " + clearChar("I love Java", 'a'));
+//        System.out.println("2. " + clearChar("Banana", 'a'));
+//        System.out.println("3. " + clearChar("Banana", 't'));
+
+        System.out.println("1. " + clearWord("I love Java", "av"));
+        System.out.println("2. " + clearWord("I love Java", "ava"));
+        System.out.println("3. " + clearWord("I love Java", "Jav"));
+        System.out.println("4. " + clearWord("Banana", "an"));
+        System.out.println("5. " + clearWord("Banana", "banananananna"));
+
+
     }
 
     /*
@@ -70,4 +82,44 @@ public class Exercise05_FindTheIndexOf {
     "Banana", 'a' -> "Bnn"
     "Banana", 't' -> "Banana"
      */
+
+    public static String clearChar(String s, char c){
+        String newS = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != c) {
+                newS += s.charAt(i);
+            }
+        }
+        return newS;
+    }
+
+     /*
+    Create a public static method named as clearWord() and it will take two Strings,
+    then it will return the String without the given second String. If the second
+    String is bigger than the first one return empty.
+
+    Example:
+
+    "I love Java", "av" ->  "I love Ja"
+    "Banana", 'an' -> "Ba"
+    "Banana", 'bananananaa' -> ""
+     */
+
+    public static String clearWord(String s, String word){
+        if (word.length() > s.length()){
+            return "";
+        }
+
+//        "I love Java", "av" -> "I love Java", " love Java", "love Java", "ove Java",...,"ava", "av", "a"
+        String newS = "";
+        for (int i = 0; i < s.length(); i++) { // i = s.length - 3
+            if (!s.substring(i).startsWith(word)){
+                newS += s.charAt(i);
+            }else {
+                i += word.length() - 1;
+            }
+        }
+
+        return newS;
+    }
 }
