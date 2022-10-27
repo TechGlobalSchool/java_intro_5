cc14e21376914b3fe135ff5374ff004d68d45ad6
Tech Global <tgschool01@gmail.com>
2022-10-26 19:38:10 -0500

    ...

--- src/arrays/_10_Find_Longest_Shortest.java
+++ src/arrays/_10_Find_Longest_Shortest.java
@@ -3,7 +3,8 @@ package arrays;
 public class _10_Find_Longest_Shortest {
 
     public static void main(String[] args) {
-
+        String[] colors = {"red", "blue", "yellow", "white"};
+        printLongestShortest(colors);
     }
 
     /*
@@ -11,11 +12,41 @@ public class _10_Find_Longest_Shortest {
     string from that array.
 
     Example:
-    Array ->  String[] colors = {"red", "blue", "yellow", "white"};
+        Array ->  String[] colors = {"red", "blue", "yellow", "white"};
 
     Output:
         Shortest = red
         Longest = yellow
-
      */
+
+    public static void printLongestShortest(String[] strings) {
+
+        /*
+        1. containers -> two one for small, another for big one
+        2. loop -> fori, increment, two if conditions (one for small, another one for the big one)
+        3. print in the end (short first)
+         */
+
+        String shortStr = strings[0], longStr = strings[0];
+
+        // {"red", "blue", "yellow", "white"}
+        for (int i = 1; i < strings.length; i++) {
+            // i =  1,           2,                     3
+            // "red" <-> "blue"| "blue" <-> "yellow"  | "yellow" <-> "white"
+            // longStr = "blue"| longStr = "yellow"   | longStr = "yellow"
+            if (longStr.length() < strings[i].length()) {
+                // my current longStr is smaller than current str reassign it.
+                longStr = strings[i];
+            }
+
+            if (shortStr.length() > strings[i].length()) {
+                // my current shortStr is longer than current str reassign it.
+                shortStr = strings[i];
+            }
+        }
+
+        System.out.println("Shortest = " + shortStr);
+        System.out.println("Longest = " + longStr);
+    }
+
 }
---
+++ src/arrays/_13_SecondGreatest_SecondSmallest.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class _13_SecondGreatest_SecondSmallest {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    create a public static method which will take an int array, find the max, second max and min, second min
+    from that array then print them out.
+
+    Hint: Find the max and min FIRST.
+
+    Note: Do it with sort and without sort.
+
+    Example:
+       Array -> int[] numbers = {2, 3, 7, 1, 1, 7};
+
+    Output:
+        max = 7
+        second max = 3
+        min = 1
+        second min= 2
+     */
+}
