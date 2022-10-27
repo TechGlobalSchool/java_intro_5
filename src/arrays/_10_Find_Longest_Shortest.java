196ea5ed6b09c54e897abf0cc658d32cf32cf896
Tech Global <tgschool01@gmail.com>
2022-10-26 19:04:09 -0500

    ...

---
+++ src/arrays/_10_Find_Longest_Shortest.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class _10_Find_Longest_Shortest {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    create a public static method which will take a String array then it will print the longest and shortest
+    string from that array.
+
+    Example:
+    Array ->  String[] colors = {"red", "blue", "yellow", "white"};
+
+    Output:
+        Shortest = red
+        Longest = yellow
+
+     */
+}
