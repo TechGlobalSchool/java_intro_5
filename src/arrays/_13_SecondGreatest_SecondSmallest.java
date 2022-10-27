9f01dcef64b24a961f94177cc6a8d800ef76d223
Tech Global <tgschool01@gmail.com>
2022-10-26 20:02:28 -0500

    ...

--- src/arrays/_13_SecondGreatest_SecondSmallest.java
+++ src/arrays/_13_SecondGreatest_SecondSmallest.java
@@ -1,9 +1,13 @@
 package arrays;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class _13_SecondGreatest_SecondSmallest {
 
     public static void main(String[] args) {
-
+        int[] numbers = {2, 3, 7, 1, 1, 7};
+        printMaxMinSecondMaxAndMin(numbers);
     }
 
     /*
@@ -23,4 +27,44 @@ public class _13_SecondGreatest_SecondSmallest {
         min = 1
         second min= 2
      */
+
+    public static int findMax(int[] numbers){
+        int max = Integer.MIN_VALUE;
+
+        for (int n : numbers) {
+            if (n > max) max = n;
+        }
+
+        return max;
+    }
+
+    public static int findMin(int[] numbers){
+        int min = Integer.MAX_VALUE;
+
+        for (int n : numbers) {
+            if (n < min) min = n;
+        }
+
+        return min;
+    }
+
+    public static void printMaxMinSecondMaxAndMin(int[] numbers){
+
+        int secondMax = Integer.MIN_VALUE, secondMin = Integer.MAX_VALUE;
+
+        for (int n : numbers) {
+            if (n > secondMax && n < findMax(numbers)){
+                secondMax = n;
+            }
+
+            if (n < secondMin && n > findMin(numbers)){
+                secondMin = n;
+            }
+        }
+
+        System.out.println("max = " + findMax(numbers));
+        System.out.println("second max = " + secondMax );
+        System.out.println("min = " + findMin(numbers));
+        System.out.println("second min = " + secondMin );
+    }
 }
