9ba57af97bee0dfdc4a54049110b62526adfca1d
Tech Global <tgschool01@gmail.com>
2022-10-29 12:04:07 -0500

    arrayList methods

--- src/arraylist_linkedlist_vector/_01_UnderstandingArrayList.java
+++ src/arraylist_linkedlist_vector/_01_UnderstandingArrayList.java
@@ -24,5 +24,60 @@ public class _01_UnderstandingArrayList {
 
 
         //4. Adding elements to specific indexes
+        names1[0] = "Alex";
+        names2.add(0, "John"); // names2.add("John");
+
+        System.out.println("\n--------Printing array vs ArrayList--------\n");
+        System.out.println("The array = " + Arrays.toString(names1)); // [Alex, null, null]
+        System.out.println("The ArrayList = " + names2); // [John]
+
+        /*
+        Add Ali and Andrii to names1
+        Add Joe and Jane to names2
+        Print the names1 and names2 once again
+         */
+
+        names1[1] = "Ali";
+        names1[2] = "Andrii";
+        names2.add("Joe");
+        names2.add("Jane");
+
+        System.out.println("\n--------Printing array vs ArrayList--------\n");
+        System.out.println("The array = " + Arrays.toString(names1)); // [Alex, Ali, Andrii]
+        System.out.println("The ArrayList = " + names2); // [John, Joe, Jane]
+
+        /*
+        Add below names to ArrayList and print it
+        Abdallah
+        Vlad
+        Saeed
+        Suzanne
+        Hazal
+        Yildiz
+        Samir
+
+        [John, Joe, Jane, Abdallah, Vlad, Saeed, Suzanne, Hazal, Yildiz, Samir]
+         */
+
+        names2.add("Abdallah");
+        names2.add("Vlad");
+        names2.add("Saeed");
+        names2.add("Suzanne");
+        names2.add("Hazal");
+        names2.add("Yildiz");
+        names2.add("Samir");
+
+        System.out.println("\n--------Printing array vs ArrayList--------\n");
+        System.out.println("The ArrayList = " + names2); // [John, Joe, Jane, Abdallah, Vlad, Saeed, Suzanne, Hazal, Yildiz, Samir]
+        System.out.println("The size of the ArrayList = " + names2.size()); // 10
+
+
+        //5. Updating an existing element in an array vs ArrayList
+        names1[1] = "Mike";
+        System.out.println(names2.set(1, "Beyza"));
+
+        System.out.println("The array = " + Arrays.toString(names1)); // [Alex, Mike, Andrii]
+        System.out.println("The ArrayList = " + names2); // [John, Olena, Jane, Abdallah, Vlad, Saeed, Suzanne, Hazal, Yildiz, Samir]
+
     }
 }
