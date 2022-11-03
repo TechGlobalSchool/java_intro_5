2fd0118080f5f163c1501102ec0c9e3a6da3eacd
Tech Global <tgschool01@gmail.com>
2022-11-02 19:49:53 -0500

    ...

--- src/practice/arrayListPractices/Contains2Or3.java
+++ src/practice/arrayListPractices/Contains2Or3.java
@@ -7,7 +7,11 @@ import java.util.ArrayList;
 public class Contains2Or3 {
 
     public static void main(String[] args) {
+        System.out.println("\n\ncontains 2 or 3");
         System.out.println(contains2Or3());
+
+        System.out.println("\n\ncontains 7 or 13");
+        System.out.println(contains7Or13());
     }
 
     /*
@@ -22,11 +26,11 @@ public class Contains2Or3 {
     [0, 2, -7, 0, 3] -> true
      */
 
-    public static boolean contains2Or3(){
+    public static boolean contains2Or3() {
         ArrayList<Integer> randoms = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
-            randoms.add(  RandomNumberGenerator.getARandomNumber(1, 10)  );
+            randoms.add(RandomNumberGenerator.getARandomNumber(1, 10));
         }
         System.out.println(randoms);
 
@@ -37,6 +41,16 @@ public class Contains2Or3 {
     create a public static method which generate 7 numbers between -20 to 20 (both included)
     then store all of them inside an arrayList and return true if it is containing
     "7" or "13".
-
      */
+
+    public static boolean contains7Or13() {
+        ArrayList<Integer> randoms = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            randoms.add(RandomNumberGenerator.getARandomNumber(-20, 20));
+        }
+        System.out.println(randoms);
+
+        return randoms.contains(7) || randoms.contains(13);
+    }
 }
---
+++ src/practice/arrayListPractices/ContainsApple.java
@@ -0,0 +1,45 @@
+package practice.arrayListPractices;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ContainsApple {
+
+    public static void main(String[] args) {
+        ArrayList<String> fruits1 = new ArrayList<>(Arrays.asList("banana", "orange", "Apple"));
+        ArrayList<String> fruits2 = new ArrayList<>(Arrays.asList("banana", "orange", "apple"));
+        ArrayList<String> fruits3 = new ArrayList<>(Arrays.asList("pineapple", "banana", "orange", "grapes"));
+
+        System.out.println(containsApple(fruits1));
+        System.out.println(containsApple(fruits2));
+        System.out.println(containsApple(fruits3));
+
+        System.out.println(containsAppleEasier(fruits1));
+        System.out.println(containsAppleEasier(fruits2));
+        System.out.println(containsAppleEasier(fruits3));
+    }
+
+    /*
+    create a public static method which will take an arraylist then it will check it is
+    containing "apple".
+
+    NOTE: IGNORE CASES
+
+    Example:
+        ArrayList -> {"banana", "orange", "Apple"} ==> true
+        ArrayList -> {"banana", "orange", "apple"} ==> true
+        ArrayList -> {"pineapple", "banana", "orange", "grapes} ==> false
+     */
+
+    public static boolean containsApple(ArrayList<String> fruits) {
+        for (String fruit : fruits) {
+            if (fruit.equalsIgnoreCase("apple")) return true;
+        }
+        return false;
+    }
+
+    public static boolean containsAppleEasier(ArrayList<String> fruits) {
+        // it will fail with -> "APPLE", "AppLe"
+       return fruits.contains("apple") || fruits.contains("Apple");
+    }
+}
